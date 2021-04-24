98609b2fa5fdef2358f9345e3404e52262b311eb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-04-24 20:45:40 +0700

    select data from table

---
+++ b2-select/b21-select-table.sql
@@ -0,0 +1,9 @@
+-- menampilkan SEMUA data di table products
+select *
+from products;
+-- menampilkan beberapa field/kolom tertentu dari table products
+select id,
+    nama,
+    harga,
+    jumlah
+from products;
