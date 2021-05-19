0a7c6bbd3e5ae1911b2eec559c10068bb3040f23
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-19 18:42:05 +0700

    finish

--- l1-user-management/l13-ubah-password-user.sql
+++ l1-user-management/l13-ubah-password-user.sql
@@ -1,3 +1,13 @@
 -- mengubah password untuk user
 set password for 'bayu' @'localhost' = 'rahasia';
 set password for 'bagaswara' @'localhost' = 'rahasia';
+select id as id,
+    name as name,
+    created_date as createdDate,
+    created_time as createdTime,
+    is_active as active,
+    counter as counter,
+    currency as currency,
+    description as description,
+    floating as floating
+from example_table
